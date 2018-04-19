07b8e6640a8d52b346e2c80a2e8295c9a0056985
n0rdream <nordream@gmail.com>
2018-04-19 19:27:49 +0300

    Added words below numbers

--- countdown/helpers.py
+++ countdown/helpers.py
@@ -7,3 +7,25 @@ def add_zero(digit):
 def get_tuple(env_string):
     color = env_string.strip().replace(' ', '')
     return tuple(int(c) for c in color.split(','))
+
+
+def get_word(num, days=False, hours=False, minutes=False):
+    if days:
+        words = ('день', 'дня', 'дней')
+    elif hours:
+        words = ('час', 'часа', 'часов')
+    elif minutes:
+        words = ('минута', 'минуты', 'минут')
+    last_digit = num[-1]
+    if last_digit == '1':
+        return words[0]
+    if last_digit in ['2', '3', '4']:
+        return words[1]
+    return words[2]
+
+
+def get_words(nums):
+    return [
+        get_word(nums[0], days=True),
+        get_word(nums[1], hours=True),
+        get_word(nums[2], minutes=True)]
--- countdown/tasks.py
+++ countdown/tasks.py
@@ -2,7 +2,7 @@ import os
 import sys
 import logging
 from dotenv import load_dotenv
-from .helpers import get_tuple
+from .helpers import get_tuple, get_words
 from .timer import get_countdown
 from .vk_requests import get_upload_url, upload_file, save_cover
 from .editors import create_cover
@@ -20,13 +20,16 @@ def update_cover():
     cover_final_path = os.path.join(MEDIA_DIR, os.getenv('COVER_FINAL_FILENAME'))
     font_path = os.path.join(MEDIA_DIR, os.getenv('FONT_FILENAME'))
     color_rgb = get_tuple(os.getenv('COLOR'))
-    color_size = int(os.getenv('COLOR_SIZE'))
+    font_size_nums = int(os.getenv('FONT_SIZE_NUMS'))
+    font_size_words = int(os.getenv('FONT_SIZE_WORDS'))
     x_coords = get_tuple(os.getenv('X_COORDINATES'))
-    y_coords = get_tuple(os.getenv('Y_COORDINATES'))
+    y_coord_nums = int(os.getenv('Y_COORDINATE_NUMS'))
+    y_coord_words = int(os.getenv('Y_COORDINATE_WORDS'))
     group_id = os.getenv('VK_GROUP_ID')
     access_token = os.getenv('VK_GROUP_ACCESS_TOKEN')
     api_version = os.getenv('VK_API_VERSION')
     log_dir = os.path.join(BASE_DIR, os.getenv('LOG_DIR'))
+
 
     logging.basicConfig(
         level=logging.INFO,
@@ -35,10 +38,14 @@ def update_cover():
         datefmt='%Y.%m.%d %H:%M:%S')
 
     try:
-        countdown = get_countdown(date_event)
+        nums = get_countdown(date_event)
         create_cover(
-            countdown, cover_orginal_path, cover_final_path,
-            font_path, color_rgb, color_size, x_coords, y_coords)
+            nums, cover_orginal_path, cover_final_path, font_path, 
+            color_rgb, font_size_nums, x_coords, y_coord_nums)
+        words = get_words(nums)
+        create_cover(
+            words, cover_final_path, cover_final_path, font_path, 
+            color_rgb, font_size_words, x_coords, y_coord_words)
         upload_url = get_upload_url(group_id, access_token, api_version)
         vk_hash, vk_photo = upload_file(cover_final_path, upload_url)
         save_cover(vk_hash, vk_photo, access_token, api_version)
